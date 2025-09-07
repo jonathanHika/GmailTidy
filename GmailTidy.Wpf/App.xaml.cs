48a146679845844437f705f18053816c472bde28
jonathanHika <jonathanhika@gmail.com>
2025-09-07 16:45:30 -0700

    Create App.xaml.csCreate App.xaml.cs

---
+++ GmailTidy.Wpf/App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace GmailTidy.Wpf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
